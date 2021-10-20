d1f3ccfe8557c1add3274196c7cc396e730077c5
winniecarole <winniedjoukeng@gmail.com>
2021-10-20 20:25:01 +0200

    update calculator

--- calculator/server.js
+++ calculator/server.js
@@ -6,8 +6,8 @@ var app = express();
 app.listen(8080, function () {
     console.log("Server gestartet auf http://localhost:8080");
 });
+app.use(express.static("public"));
 
-//app.use(express.static(__dirname + '/public'));
 
 app.use("/res", express.static(__dirname + "/view/res"));
 
@@ -16,15 +16,10 @@ app.get("/",function (req,res){
     res.sendFile(__dirname+"/index.html")
 })
 
-/*app.get("/service",function (req,res){
-    res.send("contact me at: qinnie@gmail.com");
-})*/
-
 app.get("/result/:value",function (req,res){
     var value=req.params.value;
     console.log(value)
     var res1 = eval(value);
 
     res.send(""+res1);
-
-})
+});
